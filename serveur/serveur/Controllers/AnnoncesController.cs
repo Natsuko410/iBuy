80a7e0f478acc8457bd57cdceab6073a3ffe243c
Tanguy Georges <la188522@student.helha.be>
2020-10-13 15:57:33 +0200

    Update AnnoncesController.cs
    Ajout de toutes les possibilités de recherches d'articles

--- serveur/serveur/Controllers/AnnoncesController.cs
+++ serveur/serveur/Controllers/AnnoncesController.cs
@@ -6,6 +6,7 @@ using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
 using System.Web.Http.Description;
 using serveur.Data;
@@ -18,9 +19,69 @@ namespace serveur.Controllers
         private IBuyContext db = new IBuyContext();
 
         // GET: api/Annonces
+        // ?nom=str                             nom d'un article 
+        // ?idCat=num                           id de catégorie de produit 
+        // ?limit=num                           nb d'article par page
+        // ?offset=num                          indice de la page 
+        // ?typeDeVente= enchere / achat        type de vente
         public IQueryable<Annonce> GetAnnonces()
         {
-            return db.Annonces;
+            Uri Uri = Request.RequestUri;
+            IQueryable<Annonce> Annonces = null;
+
+            int IdCat = 0;
+            int Limit = 20;
+            int Offset = 0;
+            String TypeDeVente = HttpUtility.ParseQueryString(Uri.Query).Get("typeDeVente");
+            String NomRecherche = HttpUtility.ParseQueryString(Uri.Query).Get("nom");
+
+            if (NomRecherche == null)
+                NomRecherche = "";
+
+            try
+            {
+                IdCat = int.Parse(HttpUtility.ParseQueryString(Uri.Query).Get("idCat"));
+            }
+            catch { }
+            try
+            {
+                Limit = int.Parse(HttpUtility.ParseQueryString(Uri.Query).Get("limit"));
+            }
+            catch { }
+            try
+            {
+                Offset = int.Parse(HttpUtility.ParseQueryString(Uri.Query).Get("offset"));
+            }
+            catch { }
+
+            switch (TypeDeVente)
+            {
+                case "enchere":
+                    if (IdCat != 0)
+                        Annonces = db.Annonces.Where(anno => anno.Etat == "vente" && anno.IsEnchere == true && anno.Produit.IdCat == IdCat && anno.Produit.Nom.Contains(NomRecherche)).OrderBy(anno => anno.IdAnno).Skip(Limit * Offset).Take(Limit);
+                    else
+                        Annonces = db.Annonces.Where(anno => anno.Etat == "vente" && anno.IsEnchere == true && anno.Produit.Nom.Contains(NomRecherche)).OrderBy(anno => anno.IdAnno).Skip(Limit * Offset).Take(Limit);
+
+                    break;
+
+                case "achat":
+                    if (IdCat != 0)
+                        Annonces = db.Annonces.Where(anno => anno.Etat == "vente" && anno.IsAchat == true && anno.Produit.IdCat == IdCat && anno.Produit.Nom.Contains(NomRecherche)).OrderBy(anno => anno.IdAnno).Skip(Limit * Offset).Take(Limit);
+                    else
+                        Annonces = db.Annonces.Where(anno => anno.Etat == "vente" && anno.IsAchat == true && anno.Produit.Nom.Contains(NomRecherche)).OrderBy(anno => anno.IdAnno).Skip(Limit * Offset).Take(Limit);
+                    
+                    break;
+
+                default:
+                    if (IdCat != 0)
+                        Annonces = db.Annonces.Where(anno => anno.Etat == "vente" && anno.Produit.IdCat == IdCat && anno.Produit.Nom.Contains(NomRecherche)).OrderBy(anno => anno.IdAnno).Skip(Limit * Offset).Take(Limit);
+                    else
+                        Annonces = db.Annonces.Where(anno => anno.Etat == "vente" && anno.Produit.Nom.Contains(NomRecherche)).OrderBy(anno => anno.IdAnno).Skip(Limit * Offset).Take(Limit);
+
+                    break;
+            }
+
+            return Annonces;
         }
 
         // GET: api/Annonces/5
